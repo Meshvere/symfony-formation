b176fd5702c32149101cd39a88a557a1ff21177e
Meshvere <meshus.verus@free.fr>
2019-06-11 16:18:33 +0200

    Typage réponse

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    public function index()
+    public function index(): Response
     {
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
     }
 
-    public function asJson()
+    public function asJson(): JsonResponse
     {
         return $this->json($this->getParameter('app.contact_information'));
     }
